0f3882d6e06f37bba3996ea30c9cb737e33fdf82
GhoRid <pgh5489@gmail.com>
2026-08-23 17:26:02 +0900

    feat: improve platform support handling and update package naming conventions

--- core/node/index.js
+++ core/node/index.js
@@ -1,26 +1,30 @@
-'use strict'
+"use strict";
 
 const unsupported = () => {
-  throw new Error('printer-engine native printing is currently supported only on Windows.')
-}
+  throw new Error(
+    `printer-engine native printing is not supported on ${process.platform}-${process.arch}.`,
+  );
+};
 
-if (process.platform !== 'win32') {
-  module.exports = {
-    isSupported: () => false,
-    initialize: unsupported,
-    printTest: unsupported,
-    printJson: unsupported,
-    setForms: unsupported,
-    print: unsupported,
-    getPrinterType: unsupported,
-    shutdown() {}
-  }
+const unsupportedModule = {
+  isSupported: () => false,
+  initialize: unsupported,
+  printTest: unsupported,
+  printJson: unsupported,
+  setForms: unsupported,
+  print: unsupported,
+  getPrinterType: unsupported,
+  shutdown() {},
+};
+
+if (process.platform !== "win32") {
+  module.exports = unsupportedModule;
+} else if (!["ia32", "x64"].includes(process.arch)) {
+  module.exports = unsupportedModule;
 } else {
-  if (!['ia32', 'x64'].includes(process.arch)) {
-    throw new Error(`printer-engine: Unsupported Windows architecture: ${process.arch}`)
-  }
+  const native = require(`../prebuilds/win32-${process.arch}/printer_engine_node.node`);
 
-  const native = require(`../prebuilds/win32-${process.arch}/printer_engine_node.node`)
-  native.isSupported = () => true
-  module.exports = native
+  native.isSupported = () => true;
+
+  module.exports = native;
 }
